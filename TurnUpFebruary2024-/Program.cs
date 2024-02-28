9d38da5b5a09f3f33b05ae7e3f9f62f52104ee78
Prasannalakshmi Bandi <prasannalakshmi.bandi1@gmail.com>
2024-02-28 15:00:52 +1300

    Intial code changes for turnup portal automation

---
+++ TurnUpFebruary2024-/Program.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+
+//Open Chrome/Firefox browser
+IWebDriver driver = new ChromeDriver();
+driver.Manage().Window.Maximize();
+
+//Launch TurnUp portal and navigate to website login page
+driver.Navigate().GoToUrl("http://horse.industryconnect.io/");
+
+//Identify username textbox and enter valid username
+IWebElement usernameTextbox = driver.FindElement(By.Id("UserName"));
+usernameTextbox.SendKeys("hari");
+
+//Identify password textbox and enter valid passwordI
+IWebElement PasswordTextbox = driver.FindElement(By.Id("Password"));
+PasswordTextbox.SendKeys("123123");
+
+//Identify login button and click on Login button
+IWebElement loginButton = driver.FindElement(By.XPath("//*[@id=\"loginForm\"]/form/div[3]/input[1]"));
+loginButton.Click( );
+
+//Check if the user has logged in successfully
+IWebElement helloHari = driver.FindElement(By.XPath("//*[@id=\"logoutForm\"]/ul/li/a"));
+if(helloHari.Text == "Hello hari!")
+        {
+    Console.WriteLine("user has logged in successfully");
+}
+else
+{
+    Console.WriteLine("user has not been logged in successfully :(:( ");
+
+}
